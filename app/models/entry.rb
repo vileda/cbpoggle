e4a790c409c8195c571ef7d58c636c0e95a11025
Ryan Bates <ryan@railscasts.com>
2008-10-21 20:08:51 +0200

    translation

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,6 @@
 class Entry < ActiveRecord::Base
   belongs_to :cash_book
+  belongs_to :user
   validates_numericality_of :revenue
   validates_numericality_of :expenditure
   validates_presence_of :cash_book_id
