101557f627a2ed007733e72c4ebb8a0d588356ed
vileda <vileda@viledas-macbook.nanometa.ffn>
2009-05-04 04:31:49 +0200

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  GetText.locale = 'de_DE'
-  init_gettext "cbp" 
   include AuthenticatedSystem
   helper :all # include all helpers, all the time
 
@@ -15,22 +13,5 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
-  before_filter :define_language
-
-  def define_language
-    # here you can switch the locale on fly
-    # say like that
-
-    case params[:lang]
-      when 'en': set_locale 'en_EN'
-      when 'fr': set_locale 'fr_FR'
-      when 'ru': set_locale 'ru_RU'
-      else       set_locale 'de_DE'
-    end
-  end
-  
-  private
-  
-
   
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,10 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require 'gettext/rails'
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
@@ -43,7 +42,7 @@ Rails::Initializer.run do |config|
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Uncomment to use default local time.
   config.time_zone = 'UTC'
-  Date::DATE_FORMATS[:de_time] = "%d.%m.%Y"
+  #Date::DATE_FORMATS[:de_time] = "%d.%m.%Y"
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
